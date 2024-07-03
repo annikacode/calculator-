71f2f2a4db50d57106803420b9b9adb16bd295f6
Annika <annikahenry5@gmail.com>
2024-07-03 15:13:07 +0200

    added function to multiply

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function add(a, b) {
     return a + b;
 }
 
+
+// Function to subtract
 function subtract(a, b) {
     return a - b;
 }
 
+// Function to multiply
+function multiply (a, b) {
+    return a * b;
+}
