942788f3c470c0ccefdc982b3b13245992de96c3
Annika <annikahenry5@gmail.com>
2024-07-03 15:32:31 +0200

    Added variables for first and second nummber and operator

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function subtract(a, b) {
 function multiply (a, b) {
     return a * b;
 }
+
+// Function to divide
+function divide(a, b) {
+    if (b === 0) {
+        return 'Error'
+    } 
+    return a / b;
+}
